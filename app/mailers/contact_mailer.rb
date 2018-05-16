a3e5c422d830649e26bed077e734d3d74a28ee00
tomoki ito <5cho38romu@gmail.com>
2018-05-16 11:07:19 +0900

    action mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to:5cho38romu@gmail.com,subject:"お問い合わせの確認メール"
+    mail to:"5cho38romu@gmail.com",subject:"お問い合わせの確認メール"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/lit-oasis-65314.git' }
+  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/secret-mountain-13040.git' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
